9049af5c82eaa2f52dcb8110038f57090fb3bdbc
Ariful Islam Arif <devboyarif@gmail.com>
2023-01-05 22:03:11 +0600

    login and reset password show/hide icon refactored

--- resources/js/Pages/Auth/Reset.vue
+++ resources/js/Pages/Auth/Reset.vue
@@ -24,9 +24,8 @@
                             <div class="relative">
                                 <input :disabled="!form.code" v-model="form.password" :type="passwordFieldType" id="password" :class="['border sm:text-sm text-gray-900 rounded-lg block w-full p-2.5', form.errors.password ? 'bg-white border-red-600 focus:ring-red-600 focus:border-red-600 dark:bg-red-600 dark:border-red-600 dark:placeholder-red-400 dark:text-white dark:focus:ring-red-500 dark:focus:border-blue-500':'bg-white border-gray-300 focus:ring-blue-600 focus:border-blue-600 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500']" placeholder="New Password">
                                 <span class="absolute inset-y-0 right-0 pr-2 flex items-center cursor-pointer" @click="switchVisibility" v-if="form.password">
-                                <svg v-if="passwordFieldType == 'text'" class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" /><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" /></svg>
-
-                                <svg v-else class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21" /></svg>
+                                    <EyeShowIcon v-if="passwordFieldType == 'text'"/>
+                                    <EyeHideIcon v-else/>
                                 </span>
                             </div>
                             <ErrorMessage :name="form.errors.password" />
@@ -47,7 +46,14 @@
 </template>
 
 <script>
+import EyeHideIcon from '@/Shared/Icons/EyeHideIcon.vue';
+import EyeShowIcon from '@/Shared/Icons/EyeShowIcon.vue';
+
 export default {
+    components:{
+        EyeHideIcon,
+        EyeShowIcon
+    },
     props: {
         username: String,
     },
