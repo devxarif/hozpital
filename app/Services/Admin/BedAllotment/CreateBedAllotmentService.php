920f6af9fd7cddf71c1b386d358830ea817ea0ed
Ariful Islam Arif <devboyarif@gmail.com>
2023-01-07 23:58:16 +0600

    bed allotment service added

---
+++ app/Services/Admin/BedAllotment/CreateBedAllotmentService.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Services\Admin\BedAllotment;
+
+use App\Models\BedAllotment;
+
+class CreateBedAllotmentService
+{
+    public function execute(object $request): BedAllotment
+    {
+        return BedAllotment::create([
+            'bed_id' => $request->bed,
+            'patient_id' => $request->patient,
+            'doctor_id' => $request->doctor,
+            'allotment_time' => $request->allotment_time,
+            'discharge_time' => $request->discharge_time,
+            'description' => $request->description,
+        ]);
+    }
+}
