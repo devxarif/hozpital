5a642776e15c96e8fcc2ccd0aed8a1cea83e69d4
Ariful Islam Arif <devboyarif@gmail.com>
2023-01-07 23:58:42 +0600

    blood donor service added

---
+++ app/Http/Requests/Admin/BloodBank/BloodDonorCreateRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests\Admin\BloodBank;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class BloodDonorCreateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required',
+            'blood_group' => 'required',
+            'gender' => 'required',
+            'phone' => 'required',
+            'email' => "required|email|unique:blood_donors,email",
+            'address' => 'nullable',
+            'age' => 'nullable',
+            'last_donation_date' => 'nullable',
+        ];
+    }
+}
