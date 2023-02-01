2947e5ba1f125fdd2952740d23acf51b9ab621be
Ariful Islam Arif <devboyarif@gmail.com>
2023-02-02 00:39:08 +0600

    update payment setting

--- resources/js/Pages/Admin/Setting/Payment.vue
+++ resources/js/Pages/Admin/Setting/Payment.vue
@@ -3,79 +3,114 @@
     <Head :title="__('Application')" />
 
     <SettingLayout>
-        <div class="grid gap-6 md:grid-cols-1 xl:grid-cols-1 mt-6">
-            <div class="bg-white w-full p-6 rounded-lg border border-gray-8 dark:border-gray-700 dark:hover:bg-gray-700">
+        <div class="grid gap-6 md:grid-cols-1 xl:grid-cols-12 mt-6">
+            <div class="col-span-4 bg-white w-full p-6 rounded-lg border border-gray-8 dark:border-gray-700 dark:hover:bg-gray-700">
+                <div class="overflow-hidden bg-white shadow sm:rounded-md">
+                    <ul role="list" class="space-y-1 shadow-lg">
+                        <li class="overflow-hidden rounded-md px-3.5 py-3.5 sm:px-6 text-sm border border-gray-100 cursor-pointer" :class="{'bg-blue-500 text-white': currentTab == 'paypal'}" @click="changeTab('paypal')">{{ __('Paypal') }}</li>
+                        <li class="overflow-hidden rounded-md px-3.5 py-3.5 sm:px-6 text-sm border border-gray-100 cursor-pointer" :class="{'bg-blue-500 text-white': currentTab == 'stripe'}" @click="changeTab('stripe')">{{ __('Stripe') }}</li>
+                        <li class="overflow-hidden rounded-md px-3.5 py-3.5 sm:px-6 text-sm border border-gray-100 cursor-pointer" :class="{'bg-blue-500 text-white': currentTab == 'razorpay'}" @click="changeTab('razorpay')">{{ __('Razorpay') }}</li>
+                        <li class="overflow-hidden rounded-md px-3.5 py-3.5 sm:px-6 text-sm border border-gray-100 cursor-pointer" :class="{'bg-blue-500 text-white': currentTab == 'flutterwave'}" @click="changeTab('flutterwave')">{{ __('Flutterwave') }}</li>
+                        <li class="overflow-hidden rounded-md px-3.5 py-3.5 sm:px-6 text-sm border border-gray-100 cursor-pointer" :class="{'bg-blue-500 text-white': currentTab == 'mollie'}" @click="changeTab('mollie')">{{ __('Mollie') }}</li>
+                        <li class="overflow-hidden rounded-md px-3.5 py-3.5 sm:px-6 text-sm border border-gray-100 cursor-pointer" :class="{'bg-blue-500 text-white': currentTab == 'paystack'}"  @click="changeTab('paystack')">{{ __('PayStack') }}</li>
+                        <li class="overflow-hidden rounded-md px-3.5 py-3.5 sm:px-6 text-sm border border-gray-100 cursor-pointer" :class="{'bg-blue-500 text-white': currentTab == 'instamojo'}" @click="changeTab('instamojo')">{{ __('Instamojo') }}</li>
+                        <li class="overflow-hidden rounded-md px-3.5 py-3.5 sm:px-6 text-sm border border-gray-100 cursor-pointer" :class="{'bg-blue-500 text-white': currentTab == 'midtrans'}" @click="changeTab('midtrans')">{{ __('Midtrans') }}</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-span-8 bg-white w-full p-6 rounded-lg border border-gray-8 dark:border-gray-700 dark:hover:bg-gray-700">
                 <form class="space-y-8 divide-y divide-gray-200">
                     <div class="space-y-8 divide-y divide-gray-200 sm:space-y-5">
                         <div class="space-y-6 sm:space-y-5">
-                        <div>
-                            <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Payment Setting</h2>
+                            <div>
+                                <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Paypal Payment</h2>
 
-                            <p class="mt-1 max-w-2xl text-sm text-gray-500">This information will be displayed publicly so be careful what you share.</p>
-                        </div>
-
-                        <div class="space-y-6 sm:space-y-5">
-                            <div class="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:border-t sm:border-gray-200 sm:pt-5">
-                            <label for="username" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">Username</label>
-                            <div class="mt-1 sm:col-span-2 sm:mt-0">
-                                <div class="flex max-w-lg rounded-md shadow-sm">
-                                <span class="inline-flex items-center rounded-l-md border border-r-0 border-gray-300 bg-gray-50 px-3 text-gray-500 sm:text-sm">workcation.com/</span>
-                                <input type="text" name="username" id="username" autocomplete="username" class="block w-full min-w-0 flex-1 rounded-none rounded-r-md border-gray-300 focus:border-blue-500 focus:ring-blue-500 sm:text-sm">
-                                </div>
-                            </div>
+                                <p class="mt-1 max-w-2xl text-sm text-gray-500">This information will be displayed publicly so be careful what you share.</p>
                             </div>
 
-                            <div class="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:border-t sm:border-gray-200 sm:pt-5">
-                            <label for="about" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">About</label>
-                            <div class="mt-1 sm:col-span-2 sm:mt-0">
-                                <textarea id="about" name="about" rows="3" class="block w-full max-w-lg rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm"></textarea>
-                                <p class="mt-2 text-sm text-gray-500">Write a few sentences about yourself.</p>
-                            </div>
-                            </div>
-
-                            <div class="sm:grid sm:grid-cols-3 sm:items-center sm:gap-4 sm:border-t sm:border-gray-200 sm:pt-5">
-                            <label for="photo" class="block text-sm font-medium text-gray-700">Photo</label>
-                            <div class="mt-1 sm:col-span-2 sm:mt-0">
-                                <div class="flex items-center">
-                                <span class="h-12 w-12 overflow-hidden rounded-full bg-gray-100">
-                                    <svg class="h-full w-full text-gray-300" fill="currentColor" viewBox="0 0 24 24">
-                                    <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
-                                    </svg>
-                                </span>
-                                <button type="button" class="ml-5 rounded-md border border-gray-300 bg-white py-2 px-3 text-sm font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">Change</button>
-                                </div>
-                            </div>
-                            </div>
-
-                            <div class="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:border-t sm:border-gray-200 sm:pt-5">
-                            <label for="cover-photo" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">Cover photo</label>
-                            <div class="mt-1 sm:col-span-2 sm:mt-0">
-                                <div class="flex max-w-lg justify-center rounded-md border-2 border-dashed border-gray-300 px-6 pt-5 pb-6">
-                                <div class="space-y-1 text-center">
-                                    <svg class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
-                                    <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                                    </svg>
-                                    <div class="flex text-sm text-gray-600">
-                                    <label for="file-upload" class="relative cursor-pointer rounded-md bg-white font-medium text-blue-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-blue-500 focus-within:ring-offset-2 hover:text-blue-500">
-                                        <span>Upload a file</span>
-                                        <input id="file-upload" name="file-upload" type="file" class="sr-only">
-                                    </label>
-                                    <p class="pl-1">or drag and drop</p>
+                            <div class="space-y-6 sm:space-y-5">
+                                <div class="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:border-t sm:border-gray-200 sm:pt-5">
+                                    <label for="username" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">{{ __('Paypal Mode') }}</label>
+                                    <div class="mt-1 sm:col-span-2 sm:mt-0">
+                                        <div class="flex max-w-lg rounded-md shadow-sm">
+                                            <div class="w-full bg-gray-100 p-2 rounded-lg">
+                                                <div class="account-switcher relative flex after:absolute candidate after:transition-all duration-300 after:rounded-lg">
+                                                    <div class="w-full rounded-xl" :class="form.request_for == 'me' ? 'bg-blue-500 text-white shadow-md ':'text-gray-800'">
+                                                        <input type="radio" id="radio1" name="radio" class="hidden" checked>
+                                                        <label for="radio1" class="relative z-50 rounded-md transition-all duration-300 w-full py-2 gap-2 flex items-center justify-center cursor-pointer" @click="changeLeaveRequestFor('me')">
+                                                            <UserIcon class="h-5 w-5"/>
+                                                            <span>Live</span>
+                                                        </label>
+                                                    </div>
+                                                    <div class="w-full rounded-xl" :class="form.request_for == 'others' ? 'bg-blue-500 text-white shadow-md ':'text-gray-800'">
+                                                        <input type="radio" id="radio2" name="radio" class="hidden">
+                                                        <label for="radio2" class="relative z-50 rounded-md transition-all duration-300 flex w-full py-2 gap-2 items-center justify-center cursor-pointer" @click="changeLeaveRequestFor('others')">
+                                                            <UsersIcon class="h-5 w-5"/>
+                                                            <span>Sandbox</span>
+                                                        </label>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </div>
-                                    <p class="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
                                 </div>
+                                <div class="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:border-t sm:border-gray-200 sm:pt-5">
+                                    <label for="username" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">{{ __('Paypal Client ID') }}  (Live)</label>
+                                    <div class="mt-1 sm:col-span-2 sm:mt-0">
+                                        <div class="flex max-w-lg rounded-md shadow-sm">
+                                            <input type="text" name="username" id="username" autocomplete="username" class="block w-full min-w-0 flex-1 rounded-md border-gray-300 sm:text-sm">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:border-t sm:border-gray-200 sm:pt-5">
+                                    <label for="username" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">{{ __('Paypal Secret Key') }}  (Live)</label>
+                                    <div class="mt-1 sm:col-span-2 sm:mt-0">
+                                        <div class="flex max-w-lg rounded-md shadow-sm">
+                                            <input type="text" name="username" id="username" autocomplete="username" class="block w-full min-w-0 flex-1 rounded-md border-gray-300 sm:text-sm">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:border-t sm:border-gray-200 sm:pt-5">
+                                    <label for="username" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">{{ __('Paypal Client ID') }}  (Sandbox)</label>
+                                    <div class="mt-1 sm:col-span-2 sm:mt-0">
+                                        <div class="flex max-w-lg rounded-md shadow-sm">
+                                            <input type="text" name="username" id="username" autocomplete="username" class="block w-full min-w-0 flex-1 rounded-md border-gray-300 sm:text-sm">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:border-t sm:border-gray-200 sm:pt-5">
+                                    <label for="username" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">{{ __('Paypal Secret Key') }}  (Sandbox)</label>
+                                    <div class="mt-1 sm:col-span-2 sm:mt-0">
+                                        <div class="flex max-w-lg rounded-md shadow-sm">
+                                            <input type="text" name="username" id="username" autocomplete="username" class="block w-full min-w-0 flex-1 rounded-md border-gray-300 sm:text-sm">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:border-t sm:border-gray-200 sm:pt-5">
+                                    <label for="username" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">{{ __('Paypal Status') }}</label>
+                                    <div class="mt-1 sm:col-span-2 sm:mt-0">
+                                        <div class="flex max-w-lg rounded-md shadow-sm">
+                                            <label for="checked-toggle" class="inline-flex relative items-center cursor-pointer">
+                                            <input v-model="form.status"  type="checkbox" id="checked-toggle" class="sr-only peer" checked>
+                                            <div class="w-11 h-6 bg-gray-200 rounded-full peer dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                                        </label>
+                                            <!-- <input type="text" name="username" id="username" autocomplete="username" class="block w-full min-w-0 flex-1 rounded-md border-gray-300 sm:text-sm"> -->
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                            </div>
                         </div>
-                        </div>
-
                     </div>
 
                     <div class="pt-5">
                         <div class="flex justify-end">
-                        <button type="button" class="rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">Cancel</button>
-                        <button type="submit" class="ml-3 inline-flex justify-center rounded-md border border-transparent bg-blue-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">Save</button>
+                            <button :disabled="form.processing" type="submit" class="text-white justify-center flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 font-medium rounded-lg text-md px-5 py-2.5 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 mt-3">
+                                <Loading v-if="form.processing" :messageShow="false" />
+                                <span v-else>
+                                    <svg class="inline w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256"><rect width="256" height="256" fill="none"/><polyline fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="24" points="216 72.005 104 184 48 128.005"/></svg>
+                                    {{ __('Save') }}
+                                </span>
+                            </button>
                         </div>
                     </div>
                 </form>
@@ -86,23 +121,43 @@
 
 <script>
 import SettingLayout from "@/Shared/Layout/Setting.vue";
+import { UserIcon, UsersIcon } from '@heroicons/vue/24/outline'
 
 export default {
     components: {
         SettingLayout,
+        UserIcon,
+        UsersIcon
     },
     data() {
         return {
+            currentTab: "paypal",
+
             form: this.$inertia.form({
-                title: ''
+                title: '',
+                request_for: 'me',
+                status: false
+
             }),
         };
     },
     methods: {
+        async changeTab(tab) {
+            localStorage.setItem("currentTab", tab);
+            this.currentTab = tab;
+            // let response = await axios.get(
+            //     route("settings.payment.data", {
+            //         provider: tab,
+            //     })
+            // );
 
+            // this.paymentData = response.data;
+        },
     },
     mounted() {
-
+        let tab = localStorage.getItem("currentTab") || "paypal";
+        this.currentTab = tab;
+        this.changeTab(tab);
     },
 };
 </script>
