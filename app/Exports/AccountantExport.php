fe1dc8fca08653fae8d655bb8cad1cddf7b3c6e1
Ariful Islam Arif <devboyarif@gmail.com>
2023-02-19 00:04:14 +0600

    accountant data export

---
+++ app/Exports/AccountantExport.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Exports;
+
+use App\Http\Resources\Export\AccountantExportResource;
+use App\Models\Accountant;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+
+class AccountantExport implements FromCollection, WithHeadings
+{
+    public function collection()
+    {
+        return AccountantExportResource::collection(Accountant::with('user:id,name,email')->get());
+    }
+
+    public function headings(): array
+    {
+        return [
+            'Name', 'Email', 'Phone',"Address"
+        ];
+    }
+}
