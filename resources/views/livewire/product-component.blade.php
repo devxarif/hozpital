ca487943d8b20370afe2c34e3b6fc79287996a68
Ariful Islam Arif <devboyarif@gmail.com>
2023-02-24 22:21:16 +0600

    pagination issue fix

--- resources/views/livewire/product-component.blade.php
+++ resources/views/livewire/product-component.blade.php
@@ -2,7 +2,16 @@
     <div class="lg:w-1/4 px-4 order-last lg:order-first mt-8 lg:mt-0">
         <div>
             <div class="mb-6">
-                <h4 class="font-medium text-md lg:text-lg text-dark capitalize mb-10">Search</h4>
+                <div class="flex justify-between">
+                    <h4 class="font-medium text-md lg:text-lg text-dark capitalize mb-10">Search</h4>
+
+                    <div role="status" wire:loading>
+                        <svg aria-hidden="true" class="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+                        </svg>
+                    </div>
+                </div>
                 <div class="pro-sidebar-search mb-50 mt-25">
                     <form class="border-transparent" action="#" method="get">
                         <div class="relative">
@@ -50,6 +59,7 @@
                             transform="translate(-1405.617 -646.936)" fill="currentColor"></path>
                     </g>
                 </svg><span class="ps-2.5">Filters</span></button>
+
             <div class="flex items-center justify-end">
                 <div class="flex-shrink-0 text-body text-xs md:text-sm leading-4 pe-4 md:me-6 ps-2 hidden lg:block">
                     {{ $products->total() }} items
@@ -57,8 +67,6 @@
             </div>
         </div>
         <div class="grid grid-cols-2 sm:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-x-3 lg:gap-x-5 xl:gap-x-7 gap-y-3 xl:gap-y-5 2xl:gap-y-8 ">
-
-
             @foreach ($products as $product)
                 <a href="{{ route('website.product.details') }}" class="releative group box-border overflow-hidden flex rounded-md cursor-pointer pe-0 pb-2 lg:pb-3 flex-col items-start transition duration-200 ease-in-out transform hover:-translate-y-1 md:hover:-translate-y-1.5 hover:shadow-product bg-white">
                     <div class="flex mb-3 md:mb-3.5">
--- resources/views/vendor/pagination/product-pagination.blade.php
+++ resources/views/vendor/pagination/product-pagination.blade.php
@@ -2,7 +2,7 @@
     <ul class="pagination flex flex-wrap items-center justify-center">
         @if ($paginator->onFirstPage())
             <li class="mx-2">
-                <button disabled type="button" class="cursor-not-allowed flex flex-wrap items-center justify-center  w-11 h-11 bg-white shadow text-orange leading-none transition-all hover:bg-blue-500 hover:text-white" href="#">
+                <button disabled type="button" class="cursor-not-allowed flex flex-wrap rounded-md items-center justify-center  w-11 h-11 bg-white shadow text-orange leading-none transition-all hover:bg-blue-500 hover:text-white" href="#">
                     <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                         <path fill-rule="evenodd" d="M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z" clip-rule="evenodd"></path>
                       </svg>
@@ -10,7 +10,7 @@
             </li>
         @else
             <li class="mx-2">
-                <a href="{{ $paginator->previousPageUrl() }}" class="flex flex-wrap items-center justify-center  w-11 h-11 bg-white shadow text-orange leading-none transition-all hover:bg-blue-500 hover:text-white">
+                <a href="{{ $paginator->previousPageUrl() }}" class="flex flex-wrap rounded-md items-center justify-center  w-11 h-11 bg-white shadow text-orange leading-none transition-all hover:bg-blue-500 hover:text-white">
                     <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                         <path fill-rule="evenodd" d="M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z" clip-rule="evenodd"></path>
                     </svg>
@@ -27,11 +27,11 @@
                 @foreach ($element as $page => $url)
                     @if ($page == $paginator->currentPage())
                         <li class="mx-2">
-                            <a class="flex flex-wrap items-center justify-center w-11 h-11 bg-blue-500 shadow text-white leading-none transition-all" href="#">{{ $page }}</a>
+                            <a class="flex flex-wrap rounded-md items-center justify-center w-11 h-11 bg-blue-500 shadow text-white leading-none transition-all" href="#">{{ $page }}</a>
                         </li>
                     @else
                         <li class="mx-2">
-                            <a href="{{ $url }}" class="flex flex-wrap items-center justify-center  w-11 h-11 bg-white shadow text-orange leading-none transition-all hover:bg-blue-500 hover:text-white active">{{ $page }}</a>
+                            <a href="{{ $url }}" class="flex flex-wrap rounded-md items-center justify-center  w-11 h-11 bg-white shadow text-orange leading-none transition-all hover:bg-blue-500 hover:text-white active">{{ $page }}</a>
                         </li>
                     @endif
                 @endforeach
@@ -40,7 +40,7 @@
 
         @if ($paginator->hasMorePages())
             <li class="mx-2">
-                <a href="{{ $paginator->nextPageUrl() }}" class="flex flex-wrap items-center justify-center  w-11 h-11 bg-white shadow text-orange leading-none transition-all hover:bg-blue-500 hover:text-white">
+                <a href="{{ $paginator->nextPageUrl() }}" class="flex flex-wrap rounded-md items-center justify-center  w-11 h-11 bg-white shadow text-orange leading-none transition-all hover:bg-blue-500 hover:text-white">
                     <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                         <path fill-rule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clip-rule="evenodd"></path>
                       </svg>
@@ -48,7 +48,7 @@
             </li>
         @else
             <li class="mx-2">
-                <button disabled type="button" class="cursor-not-allowed flex flex-wrap items-center justify-center  w-11 h-11 bg-white shadow text-orange leading-none transition-all hover:bg-blue-500 hover:text-white">
+                <button disabled type="button" class="cursor-not-allowed flex flex-wrap rounded-md items-center justify-center  w-11 h-11 bg-white shadow text-orange leading-none transition-all hover:bg-blue-500 hover:text-white">
                     <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                         <path fill-rule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clip-rule="evenodd"></path>
                     </svg>
