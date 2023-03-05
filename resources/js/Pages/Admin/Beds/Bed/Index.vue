a3702222b91c8dfeae000e06c8b643b853873d50
Ariful Islam Arif <devboyarif@gmail.com>
2023-03-05 23:33:39 +0600

    wip

--- resources/js/Pages/Admin/Beds/Bed/Index.vue
+++ resources/js/Pages/Admin/Beds/Bed/Index.vue
@@ -12,6 +12,15 @@
             </h2>
 
             <div class="flex items-center space-x-2 sm:space-x-3 ml-auto">
+                <div class="ml-6 hidden items-center rounded-lg bg-gray-100 p-0.5 sm:flex">
+                    <button @click="changeViewType('table')" type="button" class="rounded-md p-1.5 focus:outline-none text-gray-600 hover:bg-white hover:shadow-sm shadow-sm" :class="viewType == 'table' ? 'bg-white':''">
+                        <GridIcon/>
+                    </button>
+                    <button @click="changeViewType('card')" type="button" class="rounded-md p-1.5 focus:outline-none ml-0.5 text-gray-600 hover:bg-white hover:shadow-sm shadow-sm" :class="viewType == 'card' ? 'bg-white':''">
+                        <ListIcon/>
+                    </button>
+                </div>
+
                 <!-- Clear Filter -->
                 <ClearFilter v-if="filter.keyword || filter.bed_type || filter.bed_floor || filter.status" :href="route('admin.bed.index')"/>
 
@@ -107,7 +116,7 @@
             </div>
         </transition>
 
-        <div>
+        <div class="mb-5">
             <div class="hidden sm:block">
                 <div class="border-b border-gray-200">
                     <nav class="-mb-px flex space-x-8" aria-label="Tabs">
@@ -132,11 +141,12 @@
         <!-- Body Part  -->
         <CardSkeleton :show="loading" v-if="loading"/>
 
-        <template v-else-if="!loading && beds && beds.data.length">
-            <div class="grid gap-6 md:grid-cols-2 xl:grid-cols-4 mt-5">
+        <!-- Card View  -->
+        <template v-else-if="!loading && beds && beds.data.length && viewType == 'card'">
+            <div class="grid gap-6 md:grid-cols-2 xl:grid-cols-4">
                 <span v-for="bed in beds.data" :key="bed.id" class="block p-6 bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100   ">
                     <div class="flex flex-wrap justify-between items-start">
-                        <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">{{ bed.bed_type?.name ?? 'aa' }}</h2>
+                        <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">{{ bed.bed_type?.name ?? '' }}</h2>
 
                         <Menu as="div" class="relative inline-block text-left">
                             <div>
@@ -175,9 +185,6 @@
                     <h2 class="my-1 font-bold text-lg tracking-tight text-gray-900 ">Bed Number: {{ bed.number }}</h2>
                     <h6 class="my-1 font-bold tracking-tight text-gray-900 " v-if="bed.floor">{{ bed.floor?.name ?? '' }}</h6>
                     <h6 class="my-1 font-bold tracking-tight text-gray-900 " v-if="bed.charge"><span class="text-sm">Charge</span>: $ {{ bed.charge }}</h6>
-                    <!-- <span class="text-xs font-semibold px-2 py-1 rounded bg-green-100 text-green-800   capitalize">
-                        {{ bed.status }}
-                    </span> -->
                     <div class="mt-5">
                         <span :class="bed.status == 'alloted' ? 'bg-red-500':'bg-green-500'" class="text-white text-sm font-medium mr-2 px-3 py-2 rounded-full   capitalize">
                             {{ bed.status }}
@@ -190,6 +197,76 @@
             </div>
             <Pagination :data="beds" v-if="beds && beds.data.length && beds.total > 20" class="mt-5"/>
         </template>
+
+        <!-- Table View  -->
+        <BaseTable v-else-if="!loading && beds && beds.data.length && viewType == 'table'" :items="beds">
+            <template v-slot:head>
+                <tr class="divide-x divide-gray-200">
+                    <th class="py-3.5 pl-4 pr-4 text-left text-sm font-semibold text-gray-900 sm:pl-6">Bed Type</th>
+                    <th class="px-4 py-3.5 text-left text-sm font-semibold text-gray-900 break-words">Number</th>
+                    <th class="px-4 py-3.5 text-left text-sm font-semibold text-gray-900 break-words">Floor</th>
+                    <th class="px-4 py-3.5 text-left text-sm font-semibold text-gray-900 break-words">Charge</th>
+                    <th class="px-4 py-3.5 text-left text-sm font-semibold text-gray-900 break-words">Status</th>
+                    <th width="80px" class="py-3.5 pl-4 pr-4 text-left text-sm font-semibold text-gray-900 sm:pr-6">Action</th>
+                </tr>
+            </template>
+            <template v-slot:body>
+                <tr v-for="bed in beds.data" :key="bed.id" class="divide-x divide-gray-200">
+                    <td class="py-4 pl-4 pr-4 text-sm font-medium text-gray-900 sm:pl-6">
+                        {{ bed.bed_type?.name ?? '' }}
+                    </td>
+                    <td class="py-4 pl-4 pr-4 text-sm font-medium text-gray-900 sm:pl-6">
+                        {{ bed.number }}
+                    </td>
+                    <td class="py-4 pl-4 pr-4 text-sm font-medium text-gray-900 sm:pl-6">
+                        {{ bed.floor?.name ?? '' }}
+                    </td>
+                    <td class="py-4 pl-4 pr-4 text-sm font-medium text-gray-900 sm:pl-6">
+                        {{ bed.charge }}
+                    </td>
+                    <td class="py-4 pl-4 pr-4 text-sm font-medium text-gray-900 sm:pl-6">
+                        <span :class="bed.status == 'alloted' ? 'bg-red-500':'bg-green-500'" class="text-white text-sm font-medium mr-2 px-3 py-2 rounded-full   capitalize">
+                            {{ bed.status }}
+                        </span>
+                    </td>
+                    <td class="py-4 pl-4 pr-4 text-sm text-gray-500 sm:pr-6">
+                        <Menu as="div" class="inline-block text-left">
+                            <div>
+                                <MenuButton class="flex items-center rounded-full text-gray-400 hover:text-gray-600 focus:outline-none">
+                                    <span class="sr-only">Open options</span>
+                                    <font-awesome-icon icon="fa-solid fa-ellipsis-vertical" class="h-6 w-6"/>
+                                </MenuButton>
+                            </div>
+
+                            <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
+                                <MenuItems class="absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                    <div class="py-1 text-sm">
+                                    <MenuItem v-slot="{ active }">
+                                        <a href="javascript:void(0)" @click.prevent="editData(bed)" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'group flex items-center px-4 py-2']">
+                                            <font-awesome-icon icon="fa-solid fa-pen-to-square" class="mr-3 h-5 w-5 text-blue-500 group-hover:text-blue-500"/>
+                                            Edit
+                                        </a>
+                                    </MenuItem>
+                                    <MenuItem v-slot="{ active }">
+                                        <a href="javascript:void(0)" @click.prevent="editData(bed)" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'group flex items-center px-4 py-2']">
+                                            <font-awesome-icon icon="fa-solid fa-eye" class="mr-3 h-5 w-5 text-sky-500 group-hover:text-sky-500"/>
+                                            Details
+                                        </a>
+                                    </MenuItem>
+                                    <MenuItem v-slot="{ active }">
+                                        <a href="javascript:void(0)" @click.prevent="deleteData(bed.id)" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'group flex items-center px-4 py-2']">
+                                            <font-awesome-icon icon="fa-solid fa-trash-can" class="mr-3 h-5 w-5 text-red-500 group-hover:text-red-500"/>
+                                            Delete
+                                        </a>
+                                    </MenuItem>
+                                    </div>
+                                </MenuItems>
+                            </transition>
+                        </Menu>
+                    </td>
+                </tr>
+            </template>
+        </BaseTable>
 
         <NothingFound v-else>
             <BaseButton @click="showCreateDrawer = true" class="text-white bg-blue-600 hover:bg-blue-700 px-4 py-2">
@@ -234,6 +311,7 @@
         },
         data() {
             return {
+                viewType: 'card',
                 showCreateDrawer: false,
                 showEditBed: false,
                 editBed: '',
@@ -253,6 +331,10 @@
             }
         },
         methods: {
+            changeViewType(type){
+                this.viewType = type
+                localStorage.setItem("adminBed", this.viewType);
+            },
             deleteData(id) {
                 this.$swal({
                     title: "Are you sure?",
@@ -316,6 +398,7 @@
         },
         created() {
             this.showFilter = localStorage.getItem("adminBed") == "true" ? true: false;
+            this.viewType = localStorage.getItem("adminBed") == "card" ? 'card': 'table';
         },
     };
 </script>
