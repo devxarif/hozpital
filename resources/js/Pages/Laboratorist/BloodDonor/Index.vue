a510ebb539671156e898fb3ec64e125083ca0607
Ariful Islam Arif <devboyarif@gmail.com>
2023-03-06 19:23:51 +0600

    blood donation view type update

--- resources/js/Pages/Laboratorist/BloodDonor/Index.vue
+++ resources/js/Pages/Laboratorist/BloodDonor/Index.vue
@@ -12,6 +12,15 @@
             </h2>
 
             <div class="flex items-center space-x-2 sm:space-x-3 ml-auto">
+                <div class="ml-6 hidden items-center rounded-lg bg-gray-100 p-0.5 sm:flex">
+                    <button @click="changeViewType('table')" type="button" class="rounded-md p-1.5 focus:outline-none text-gray-600 hover:bg-white hover:shadow-sm shadow-sm" :class="viewType == 'table' ? 'bg-white':''">
+                        <GridIcon/>
+                    </button>
+                    <button @click="changeViewType('card')" type="button" class="rounded-md p-1.5 focus:outline-none ml-0.5 text-gray-600 hover:bg-white hover:shadow-sm shadow-sm" :class="viewType == 'card' ? 'bg-white':''">
+                        <ListIcon/>
+                    </button>
+                </div>
+
                 <!-- Clear Filter -->
                 <ClearFilter v-if="filter.keyword || filter.gender || filter.blood_group" :href="route('laboratorist.bloodDonor.index')"/>
 
@@ -34,19 +43,19 @@
                         <MenuItems class="absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                             <div class="py-1 text-sm">
                             <MenuItem v-slot="{ active }">
-                                <a href="javascript:void(0)" @click.prevent="editData(department)" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'group flex items-center px-4 py-2']">
+                                <a href="javascript:void(0)" @click.prevent="editData(blood_donor)" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'group flex items-center px-4 py-2']">
                                     <font-awesome-icon icon="fa-solid fa-plus" class="h-4 w-4 mr-2"/>
                                     As PDF
                                 </a>
                             </MenuItem>
                             <MenuItem v-slot="{ active }">
-                                <a href="javascript:void(0)" @click.prevent="editData(department)" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'group flex items-center px-4 py-2']">
+                                <a href="javascript:void(0)" @click.prevent="editData(blood_donor)" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'group flex items-center px-4 py-2']">
                                     <font-awesome-icon icon="fa-solid fa-trash-can" class="h-4 w-4 mr-2 "/>
                                     As Excel
                                 </a>
                             </MenuItem>
                             <MenuItem v-slot="{ active }">
-                                <a href="javascript:void(0)" @click.prevent="editData(department)" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'group flex items-center px-4 py-2']">
+                                <a href="javascript:void(0)" @click.prevent="editData(blood_donor)" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'group flex items-center px-4 py-2']">
                                     <font-awesome-icon icon="fa-solid fa-trash-can" class="h-4 w-4 mr-2 "/>
                                     As CSV
                                 </a>
@@ -99,7 +108,8 @@
         <!-- Body Part  -->
         <CardSkeleton :show="loading" v-if="loading"/>
 
-       <template v-else-if="!loading && blood_donors && blood_donors.data.length">
+        <!-- Card View  -->
+       <template v-else-if="!loading && blood_donors && blood_donors.data.length && viewType == 'card'">
             <div class="grid gap-6 md:grid-cols-2 xl:grid-cols-4">
                 <span v-for="blood_donor in blood_donors.data" :key="blood_donor.id" class="block p-6 bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100   ">
                     <div class="flex flex-wrap justify-between items-start">
@@ -151,6 +161,74 @@
             <Pagination :data="blood_donors" v-if="blood_donors && blood_donors.data.length && blood_donors.total > 20" class="mt-5"/>
        </template>
 
+        <!-- Table View  -->
+        <BaseTable v-else-if="!loading && blood_donors && blood_donors.data.length && viewType == 'table'" :items="blood_donors">
+            <template v-slot:head>
+                <tr class="divide-x divide-gray-200">
+                    <th class="py-3.5 pl-4 pr-4 text-left text-sm font-semibold text-gray-900 sm:pl-6">Name</th>
+                    <th class="px-4 py-3.5 text-left text-sm font-semibold text-gray-900 break-words">Email</th>
+                    <th class="px-4 py-3.5 text-left text-sm font-semibold text-gray-900 break-words">Phone</th>
+                    <th class="px-4 py-3.5 text-left text-sm font-semibold text-gray-900 break-words">Gender</th>
+                    <th class="px-4 py-3.5 text-left text-sm font-semibold text-gray-900 break-words">Age</th>
+                    <th width="80px" class="py-3.5 pl-4 pr-4 text-left text-sm font-semibold text-gray-900 sm:pr-6">Action</th>
+                </tr>
+            </template>
+            <template v-slot:body>
+                <tr v-for="blood_donor in blood_donors.data" :key="blood_donor.id" class="divide-x divide-gray-200">
+                    <td class="p-4 text-sm text-gray-500 break-all">
+                        {{ blood_donor.name }}
+                    </td>
+                    <td class="p-4 text-sm text-gray-500 break-all">
+                        {{ blood_donor.email }}
+                    </td>
+                    <td class="p-4 text-sm text-gray-500 break-all">
+                        {{ blood_donor.phone }}
+                    </td>
+                    <td class="p-4 text-sm text-gray-500 break-all capitalize">
+                        {{ blood_donor.gender }}
+                    </td>
+                    <td class="p-4 text-sm text-gray-500 break-all">
+                        {{ blood_donor.age }} Years old
+                    </td>
+                    <td class="py-4 pl-4 pr-4 text-sm text-gray-500 sm:pr-6">
+                        <Menu as="div" class="inline-block text-left">
+                            <div>
+                                <MenuButton class="flex items-center rounded-full text-gray-400 hover:text-gray-600 focus:outline-none">
+                                    <span class="sr-only">Open options</span>
+                                    <font-awesome-icon icon="fa-solid fa-ellipsis-vertical" class="h-6 w-6"/>
+                                </MenuButton>
+                            </div>
+
+                            <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
+                                <MenuItems class="absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                    <div class="py-1 text-sm">
+                                    <MenuItem v-slot="{ active }">
+                                        <a href="javascript:void(0)" @click.prevent="editData(blood_donor)" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'group flex items-center px-4 py-2']">
+                                            <font-awesome-icon icon="fa-solid fa-pen-to-square" class="mr-3 h-5 w-5 text-blue-500 group-hover:text-blue-500"/>
+                                            Edit
+                                        </a>
+                                    </MenuItem>
+                                    <MenuItem v-slot="{ active }">
+                                        <a href="javascript:void(0)" @click.prevent="editData(blood_donor)" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'group flex items-center px-4 py-2']">
+                                            <font-awesome-icon icon="fa-solid fa-eye" class="mr-3 h-5 w-5 text-sky-500 group-hover:text-sky-500"/>
+                                            Details
+                                        </a>
+                                    </MenuItem>
+                                    <MenuItem v-slot="{ active }">
+                                        <a href="javascript:void(0)" @click.prevent="deleteData(blood_donor.id)" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'group flex items-center px-4 py-2']">
+                                            <font-awesome-icon icon="fa-solid fa-trash-can" class="mr-3 h-5 w-5 text-red-500 group-hover:text-red-500"/>
+                                            Delete
+                                        </a>
+                                    </MenuItem>
+                                    </div>
+                                </MenuItems>
+                            </transition>
+                        </Menu>
+                    </td>
+                </tr>
+            </template>
+        </BaseTable>
+
         <NothingFound v-else>
             <BaseButton @click="showCreateDrawer = true" class="text-white bg-blue-600 hover:bg-blue-700 px-4 py-2">
                 <font-awesome-icon icon="fa-solid fa-plus" class="h-4 w-4 mr-2"/>
@@ -186,6 +264,7 @@
         },
         data() {
             return {
+                viewType: 'card',
                 showCreateDrawer: false,
                 showEditDrawer: false,
                 editBloodDonor: '',
@@ -212,6 +291,10 @@
             }
         },
         methods: {
+            changeViewType(type){
+                this.viewType = type
+                localStorage.setItem("laboratoristBloodDonorViewType", this.viewType);
+            },
             deleteData(id) {
                 this.$swal({
                     title: "Are you sure?",
@@ -250,6 +333,7 @@
         },
         created() {
             this.showFilter = localStorage.getItem("laboratoristBloodDonor") == "true" ? true: false;
+            this.viewType = localStorage.getItem("laboratoristBloodDonorViewType") == "card" ? 'card': 'table';
         },
     };
 </script>
