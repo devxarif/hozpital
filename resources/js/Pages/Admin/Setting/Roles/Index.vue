2d4789c5149a826b4b2909aa57a9b2ad18dfd170
Ariful Islam Arif <devboyarif@gmail.com>
2023-03-16 22:50:49 +0600

    update role design

--- resources/js/Pages/Admin/Setting/Roles/Index.vue
+++ resources/js/Pages/Admin/Setting/Roles/Index.vue
@@ -20,47 +20,23 @@
                             <div class="sm:border-t sm:border-gray-200 sm:pt-5">
                                 <div class="grid gap-6 md:grid-cols-2 xl:grid-cols-4">
                                     <span v-for="role in roles" :key="role.id" class="block p-6 bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100">
-                                        <div class="flex flex-wrap justify-between items-start">
-                                            <h2 class="mb-2 text-xl font-bold tracking-tight text-gray-900 ">{{ role.name }}</h2>
-
-                                            <Menu as="div" class="relative inline-block text-left">
-                                                <div>
-                                                    <MenuButton class="flex items-center rounded-full text-gray-400 hover:text-gray-600 focus:outline-none">
-                                                        <span class="sr-only">Open options</span>
-                                                        <font-awesome-icon icon="fa-solid fa-ellipsis-vertical" class="h-6 w-6"/>
-                                                    </MenuButton>
-                                                </div>
-
-                                                <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
-                                                    <MenuItems class="absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                                                        <div class="py-1 text-sm">
-                                                        <MenuItem v-slot="{ active }">
-                                                            <a href="javascript:void(0)" @click.prevent="editData(role)" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'group flex items-center px-4 py-2']">
-                                                                <font-awesome-icon icon="fa-solid fa-pen-to-square" class="mr-3 h-5 w-5 text-blue-500 group-hover:text-blue-500"/>
-                                                                Edit
-                                                            </a>
-                                                        </MenuItem>
-                                                        <MenuItem v-slot="{ active }">
-                                                            <a href="javascript:void(0)" @click.prevent="detailsData(role)" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'group flex items-center px-4 py-2']">
-                                                                <font-awesome-icon icon="fa-solid fa-eye" class="mr-3 h-5 w-5 text-sky-500 group-hover:text-sky-500"/>
-                                                                View Permissions
-                                                            </a>
-                                                        </MenuItem>
-                                                        <MenuItem v-slot="{ active }">
-                                                            <a href="javascript:void(0)" @click.prevent="deleteData(role.id)" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'group flex items-center px-4 py-2']">
-                                                                <font-awesome-icon icon="fa-solid fa-trash-can" class="mr-3 h-5 w-5 text-red-500 group-hover:text-red-500"/>
-                                                                Delete
-                                                            </a>
-                                                        </MenuItem>
-                                                        </div>
-                                                    </MenuItems>
-                                                </transition>
-                                            </Menu>
-                                        </div>
+                                        <h2 class="mb-2 text-xl font-bold tracking-tight text-gray-900 ">{{ role.name }}</h2>
                                         <p class="mb-3 font-normal text-gray-700 ">
                                             {{ role.description }}
                                         </p>
-                                        <h2 class="mb-2 text-2xl font-bold tracking-tight text-blue-500 ">{{ role.permissions.length }} Permissions Assign</h2>
+
+                                        <div class="flex gap-3 mt-4">
+                                            <a v-tooltip="'View Permissions'" href="javascript:void(0)" @click.prevent="detailsData(role)" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'group flex items-center py-2']">
+                                                <font-awesome-icon icon="fa-solid fa-eye" class=" h-5 w-5 text-sky-500 group-hover:text-sky-500" />
+                                            </a>
+                                            <a v-tooltip="'Edit'" href="javascript:void(0)" @click.prevent="editData(role)" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'group flex items-center py-2']">
+                                                <font-awesome-icon icon="fa-solid fa-pen-to-square" class=" h-5 w-5 text-blue-500 group-hover:text-blue-500" />
+                                            </a>
+                                            <a v-tooltip="'Delete'" href="javascript:void(0)" @click.prevent="deleteData(role.id)" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'group flex items-center py-2']">
+                                                <font-awesome-icon icon="fa-solid fa-trash-can" class=" h-5 w-5 text-red-500 group-hover:text-red-500" />
+                                            </a>
+                                        </div>
+
                                     </span>
                                 </div>
                             </div>
